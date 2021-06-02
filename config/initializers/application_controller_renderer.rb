34cb800a33b6a76cfb0c44244838a8abc976b7fa
tomoyukikubota <tomoyukikubota0119@gmail.com>
2021-06-02 10:15:39 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6f4cfd2396eaabfd25875fb82c86655c80c6ad79f1952d39799ac5adf805839d4bd4d616144c8a03394d24fdef619a11537ed1ba953faa18b26da1e7622cefae'
